3f7fe941d3ad2684e3264d74e6cbea07997e7447
sagrvma <notsagarverma@gmail.com>
2025-06-08 17:11:20 +0530

    Added sign up alternate in the same sign in form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,43 @@
+import { useState } from "react";
 import Header from "./Header";
 import "./Login.css";
 
 const Login = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const handleSignUp = () => {
+    setIsSignUp(!isSignUp);
+  };
+
   return (
     <div className="login">
       <Header />
       <div className="login-container">
         <form className="login-form" onSubmit={(e) => e.preventDefault()}>
-          <p>Sign In</p>
-          <input
-            type="email"
-            placeholder="Email Address"
-            className="login-email-input"
-            required
-          ></input>
-          <input
-            type="password"
-            placeholder="Password"
-            className="login-password-input"
-            required
-          ></input>
+          <p className="login-heading ">{isSignUp ? "Sign Up" : "Sign In"}</p>
+          {isSignUp && (
+            <div>
+              <input type="text" placeholder="First Name" required></input>
+              <input type="text" placeholder="Last Name" required></input>
+            </div>
+          )}
+          <input type="email" placeholder="Email Address" required></input>
+          <input type="password" placeholder="Password" required></input>
+          {isSignUp && (
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              required
+            ></input>
+          )}
           <button type="submit" className="login-button">
-            Sign In
+            {isSignUp ? "Sign Up" : "Sign In"}
           </button>
+          <p onClick={handleSignUp} className="signup-text">
+            {isSignUp
+              ? "Already a user? Sign In now."
+              : "New to Netflix? Sign up now."}
+          </p>
         </form>
       </div>
     </div>
