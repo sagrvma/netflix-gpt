121b298c3327dd8429cacac1affcf475edd32236
sagrvma <notsagarverma@gmail.com>
2025-06-09 21:45:03 +0530

    css completed for the login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,34 +1,109 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Header from "./Header";
 import "./Login.css";
 
 const Login = () => {
   const [isSignUp, setIsSignUp] = useState(false);
+  const [errors, setErrors] = useState({});
+
+  const firstName = useRef(null);
+  const lastName = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const confirmPassword = useRef(null);
 
   const handleSignUp = () => {
     setIsSignUp(!isSignUp);
+    setErrors({});
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    validateForm();
+  };
+
+  const validateForm = () => {
+    const newErrors = {};
+
+    if (!email.current.value) {
+      newErrors.email = "Enter your email address.";
+    }
+
+    if (!password.current.value) {
+      newErrors.password = "Enter password.";
+    }
+
+    if (isSignUp) {
+      if (!firstName.current.value.trim()) {
+        newErrors.firstName = "Enter your first name.";
+      }
+
+      if (!lastName.current.value.trim()) {
+        newErrors.lastName = "Enter your last name.";
+      }
+
+      if (!confirmPassword.current.value) {
+        newErrors.confirmPassword = "Enter password again.";
+      } else if (confirmPassword.current.value !== password.current.value) {
+        newErrors.confirmPassword = "Passwords don't match";
+      }
+    }
+
+    setErrors(newErrors);
   };
 
   return (
     <div className="login">
       <Header />
       <div className="login-container">
-        <form className="login-form" onSubmit={(e) => e.preventDefault()}>
+        <form className="login-form" onSubmit={handleSubmit}>
           <p className="login-heading ">{isSignUp ? "Sign Up" : "Sign In"}</p>
           {isSignUp && (
             <div>
-              <input type="text" placeholder="First Name" required></input>
-              <input type="text" placeholder="Last Name" required></input>
+              <div>
+                <input
+                  ref={firstName}
+                  type="text"
+                  placeholder="First Name"
+                ></input>
+                {errors.firstName && (
+                  <p className="validation-error-message">{errors.firstName}</p>
+                )}
+              </div>
+
+              <div>
+                <input
+                  ref={lastName}
+                  type="text"
+                  placeholder="Last Name"
+                ></input>
+                {errors.lastName && (
+                  <p className="validation-error-message">{errors.lastName}</p>
+                )}
+              </div>
             </div>
           )}
-          <input type="email" placeholder="Email Address" required></input>
-          <input type="password" placeholder="Password" required></input>
+          <input ref={email} type="email" placeholder="Email Address"></input>
+          {errors.email && (
+            <p className="validation-error-message">{errors.email}</p>
+          )}
+          <input ref={password} type="password" placeholder="Password"></input>
+          {errors.password && (
+            <p className="validation-error-message">{errors.password}</p>
+          )}
           {isSignUp && (
-            <input
-              type="password"
-              placeholder="Confirm Password"
-              required
-            ></input>
+            <div>
+              <input
+                ref={confirmPassword}
+                type="password"
+                placeholder="Confirm Password"
+              ></input>
+              {errors.confirmPassword && (
+                <p className="validation-error-message">
+                  {errors.confirmPassword}
+                </p>
+              )}
+            </div>
           )}
           <button type="submit" className="login-button">
             {isSignUp ? "Sign Up" : "Sign In"}
