5c765623538b9063baa0042e03d42010e70ebfb2
sagrvma <notsagarverma@gmail.com>
2025-06-12 22:40:29 +0530

    added signout button, only visible when signed in, navigates to / when signed out

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,34 @@
 import { netflixHeaderLogo } from "../utils/imageURLS";
 import "./Header.css";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const user = useSelector((store) => store.user);
+
+  const handleLogOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+        navigate("/error");
+      });
+  };
+
   return (
     <div className="header">
       <img src={netflixHeaderLogo} className="header-logo" />
+      {user && (
+        <button className="signout-button" onClick={handleLogOut}>
+          Sign Out
+        </button>
+      )}
     </div>
   );
 };
