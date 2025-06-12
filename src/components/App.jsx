5c0a16fe6f7d073cc0f35f61062ed3a24dbbb04a
sagrvma <notsagarverma@gmail.com>
2025-06-12 09:00:15 +0530

    added redux logic to add user data to user slice when logged in and remove when logged out

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,27 @@ import "./App.css";
 import Login from "./Login";
 import Browse from "./Browse";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        //User is signed in
+        const { uid, email, displayName } = user;
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
+      } else {
+        //User is signed out
+        dispatch(removeUser());
+      }
+    });
+  }, []);
+
   const appRouter = createBrowserRouter([
     {
       path: "/",
