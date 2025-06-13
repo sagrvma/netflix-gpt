561a78680745c1fc0f463a4c2824848e15ec1ead
sagrvma <notsagarverma@gmail.com>
2025-06-13 07:44:42 +0530

    removed persistence from firebase.js as since we fixed the routing, its no longer needed

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,11 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {
-  browserSessionPersistence,
-  getAuth,
-  setPersistence,
-} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,11 +22,3 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 export const auth = getAuth();
-setPersistence(auth, browserSessionPersistence)
-  .then(() => {
-    //Auth will clear when the browser is closed
-  })
-  .catch((error) => {
-    //error
-    console.log(error);
-  });
