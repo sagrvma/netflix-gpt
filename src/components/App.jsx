4fcb814fc17cf58a35b7e2fd6eeb6b20622e5271
sagrvma <notsagarverma@gmail.com>
2025-06-13 07:45:43 +0530

    cleaned up unnecessary imports from App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,7 @@ import "./App.css";
 import Login from "./Login";
 import Browse from "./Browse";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../utils/firebase";
-import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { addUser, removeUser } from "../utils/userSlice";
 
 const App = () => {
   const dispatch = useDispatch();
