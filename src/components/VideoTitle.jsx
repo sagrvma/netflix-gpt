a774d09f2fa325553da4d0d89f4dd982e9fb950e
sagrvma <notsagarverma@gmail.com>
2025-06-16 22:38:45 +0530

    layout for video-title nailed down

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -1,10 +1,14 @@
 const VideoTitle = ({ title, description }) => {
   return (
     <div className="video-title">
-      <h1 className="title">{title}</h1>
-      <p className="description">{description}</p>
-      <button className="play-button">Play</button>
-      <button className="more-info-button">More Info</button>
+      <div className="video-title-text">
+        <h1 className="title">{title}</h1>
+        <p className="description">{description}</p>
+      </div>
+      <div className="video-title-buttons">
+        <button className="play-button">▶ Play</button>
+        <button className="more-info-button">More Info</button>
+      </div>
     </div>
   );
 };
