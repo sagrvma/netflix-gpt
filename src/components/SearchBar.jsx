6e6cb593d9c40bda4171900755a5cf229cec4625
sagrvma <notsagarverma@gmail.com>
2025-06-20 22:23:55 +0530

    extracted fetching movies from gemini and then tdbm from searchBar to a custom hook

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,55 +1,22 @@
 import { useRef } from "react";
-import ai from "../utils/gemini";
-import { API_OPTIONS } from "../utils/constants";
-import { addSearchedMovies } from "../utils/searchSlice";
-import { useDispatch, useSelector } from "react-redux";
+import useMovieSuggestions from "../hooks/useMovieSuggestions";
+import { useSelector } from "react-redux";
 
 const SearchBar = () => {
   const searchInput = useRef(null);
-  const dispatch = useDispatch();
 
-  const getMovieTMDB = async (movie) => {
-    const searchedMovie = await fetch(
-      "https://api.themoviedb.org/3/search/movie?query=" +
-        movie +
-        "&include_adult=false&language=en-US&page=1",
-      API_OPTIONS
-    );
-    const data = await searchedMovie.json();
-
-    const finalMovie = data.results.reduce(
-      (maxPopularityMovie, currentMovie) => {
-        return currentMovie.popularity > maxPopularityMovie.popularity
-          ? currentMovie
-          : maxPopularityMovie;
-      }
-    );
-
-    return finalMovie;
-  };
+  const getGeminiMovies = useMovieSuggestions();
 
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
   const handleSearchClick = async () => {
-    const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
-      contents:
-        "Give me a list of 5 movies for the following prompt" +
-        searchInput.current.value +
-        " in the following format, no other words, only the movie names : Pulp Fiction, Reservoir Dogs, Django Unchained, The Hateful Eight, Kill Bill",
-    });
-
-    const movieSuggestions = response.text.split(", ");
-
-    const promiseArray = movieSuggestions.map((movie) => getMovieTMDB(movie));
-
-    const tmdbMovies = await Promise.all(promiseArray);
-    console.log(promiseArray);
-
-    dispatch(addSearchedMovies(tmdbMovies));
+    getGeminiMovies(searchInput.current.value);
   };
+
+  const movieRedux = useSelector((store) => store.search.searchedMovies);
+  console.log(movieRedux);
 
   return (
     <div className="search-bar">
--- src/hooks/useMovieSuggestions.js
+++ src/hooks/useMovieSuggestions.js
@@ -1,7 +1,50 @@
+import { useDispatch } from "react-redux";
+import ai from "../utils/gemini";
 import { API_OPTIONS } from "../utils/constants";
+import { addSearchedMovies } from "../utils/searchSlice";
 
 const useMovieSuggestions = () => {
-  getSuggestedMovie();
+  const dispatch = useDispatch();
+  const getMovieTMDB = async (movie) => {
+    const searchedMovie = await fetch(
+      "https://api.themoviedb.org/3/search/movie?query=" +
+        movie +
+        "&include_adult=false&language=en-US&page=1",
+      API_OPTIONS
+    );
+    const data = await searchedMovie.json();
+
+    const finalMovie = data.results.reduce(
+      (maxPopularityMovie, currentMovie) => {
+        return currentMovie.popularity > maxPopularityMovie.popularity
+          ? currentMovie
+          : maxPopularityMovie;
+      }
+    );
+
+    return finalMovie;
+  };
+
+  const getGeminiMovies = async (userPrompt) => {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents:
+        "Give me a list of 5 movies for the following prompt" +
+        userPrompt +
+        " in the following format, no other words, only the movie names : Pulp Fiction, Reservoir Dogs, Django Unchained, The Hateful Eight, Kill Bill",
+    });
+
+    const movieSuggestions = response.text.split(", ");
+
+    const promiseArray = movieSuggestions.map((movie) => getMovieTMDB(movie));
+
+    const tmdbMovies = await Promise.all(promiseArray);
+    console.log(promiseArray);
+
+    dispatch(addSearchedMovies(tmdbMovies));
+  };
+
+  return getGeminiMovies;
 };
 
 export default useMovieSuggestions;
